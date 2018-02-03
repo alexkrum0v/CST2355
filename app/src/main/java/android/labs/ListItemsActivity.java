d8ddea021adacfb1db2be71ff3796857afa64daa
alexkrum0v <alexkrumov@gmail.com>
2018-02-03 11:06:46 -0500

    Finished Lab3

--- app/src/main/java/android/labs/ListItemsActivity.java
+++ app/src/main/java/android/labs/ListItemsActivity.java
@@ -2,12 +2,40 @@ package android.labs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ListItemsActivity extends Activity {
+    protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_items);
+        setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME,"onCreate()");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "onResume()");
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "onStart()");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "onPause()");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "onStop()");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "onDestroy()");
     }
 }
--- app/src/main/java/android/labs/LoginActivity.java
+++ app/src/main/java/android/labs/LoginActivity.java
@@ -1,13 +1,57 @@
 package android.labs;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+    Button button = (Button) findViewById(R.id.button2);
+    EditText editText = (EditText) findViewById(R.id.plain_text_input1)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME,"onCreate()");
+        final SharedPreferences sharedP = getSharedPreferences("LogFile",MODE_PRIVATE);
+        sharedP.edit().putString("DefaultMail", "email@domain.com");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(sharedP.getString("DefaultMail", "email@domain.com"));
+            }
+        });
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "onResume()");
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "onStart()");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "onPause()");
+}
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "onStop()");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "onDestroy()");
     }
 }
